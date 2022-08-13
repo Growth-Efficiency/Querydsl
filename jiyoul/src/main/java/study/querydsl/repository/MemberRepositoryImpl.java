b7916ff190679ae810ca9f66455c91c416130069
JiYoul <wldbf97@naver.com>
2022-08-13 17:46:53 +0900

    [6일차] QuerydslPredicateExecutor, QuerydslRepositorySupport 등 학습

--- jiyoul/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ jiyoul/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -23,8 +24,8 @@ import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
 
+    private final JPAQueryFactory queryFactory;
     public MemberRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
     }
